47ab40c75d271ec8b2472291b2fa6a6515b4a5f3
lviguier <leo.vgr@hotmail.com>
2026-07-17 14:31:46 +0200

    Add basic prefabs + fix dungeon kit + add render props

--- src/Main.hx
+++ src/Main.hx
@@ -1,18 +1,27 @@
 class Main extends hxd.App {
-
-	override function init() {
-		var resource = hxd.Res.test;
-		var template = resource.load();
-		var cache = @:privateAccess hxd.res.Loader.currentInstance.cache;
-		trace(cache);
-		trace(template);
-		var cloned = template.make(s3d);
-		var box = cloned.find(hrt.prefab.l3d.Box);
-		box.name += "Changed";
-	}
+	var event = new hxd.WaitEvent();
 
 	static function main() {
-		hxd.Res.initEmbed();
+		hxd.Res.initLocal();
 		new Main();
+	}
+
+	override function init() {
+		super.init();
+		new h3d.scene.CameraController.OrbitCameraController(20.0, s3d);
+		loadPrefab("Prefabs/Dungeon01.prefab");
+	}
+
+	override function update(dt:Float) {
+		super.update(dt);
+		event.update(dt);
+	}
+
+	function loadPrefab(?path: String) {
+		var res = hxd.Res.load(path);
+		var sceneData = res.toPrefab().load();
+		var sceneRoot = new h3d.scene.Object(s3d);
+		var shared = new hrt.prefab.ContextShared(path, null, sceneRoot);
+		sceneData = sceneData.make(shared);
 	}
 }
